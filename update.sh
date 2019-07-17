0efed8b1c837f76391937d27d1eb92b26ba1a0fd
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-07-17 10:46:38 +0800

    2019年 07月 17日 星期三 10:46:38 CST badcw

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 cd blog && mkdocs gh-deploy
 cd /home/badcw/PycharmProjects/blog && git pull
 cd /home/badcw/PycharmProjects/blog && git add *
-timenow=$(date)
-cd /home/badcw/PycharmProjects/blog && git commit -ma "\"${timenow} badcw\""
+timenow=$(date)" badcw"
+cd /home/badcw/PycharmProjects/blog && git commit -m "${timenow}"
 cd /home/badcw/PycharmProjects/blog && git push
