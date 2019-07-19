52639cc43fa3386a6804756c8f656ce8cda5b7fc
Verly <Verly0206@gmail.com>
2019-07-19 15:44:58 +0800

    Linear Basis update

--- templates/01_Math/05_Linear Basis/01_Linear Basis.cpp
+++ templates/01_Math/05_Linear Basis/01_Linear Basis.cpp
@@ -4,23 +4,16 @@ struct LinearBasis
 {
     ll d[64], tot;
 
-    void init()
-    {
-        for(int i = 0; i < 64; i ++) d[i] = 0;
-        tot = 0;
-    }
-
-    bool ins(ll x)  //插入线性基
+    void ins(ll x)  //插入线性基
     {
         for(int i = 63; i >= 0; i --)
         {
             if((x >> i) & 1)
             {
-                if(d[i]) x ^= d[i];
-                else { d[i] = x; tot ++; return true; } //插入成功
+                if(!d[i]) return void(d[i] = x);
+                x ^= d[i];
             }
         }
-        return false;
     }
 
     ll max_xor()    //在一个序列中取若干个数，使其异或和最大
@@ -31,24 +24,21 @@ struct LinearBasis
         return ans;
     }
 
-    void work()
+    void init()
     {
-        for(int i = 1; i < 64; i ++) 
-            for(int j = 1; j <= i; j ++)
-                if(d[i] & (1 << (j - 1))) d[i] ^= d[j - 1];
+        for(int i = 0; i < 64; i ++) if(d[i])
+            for(int j = 0; j < i; j ++)
+                if(d[i] & (1ll << j)) d[i] ^= d[j];
+        for(int i = 0; i < 64; i ++) if(d[i]) d[tot ++] = d[i];
     }
 
     ll k_th(ll k)   //取任意个元素进行异或的第k小个数
     {
         //考虑能异或出0的情况，tot表示线性基中的元素个数
-        if(k == 1 && tot < n) return 0; 
-        if(tot < n) k --;
-        work();
+        k -= (n != tot);
+        if(k > (1ll << tot)) return -1;
         ll ans = 0;
-        for(int i = 0; i < 64; i ++) if(d[i] != 0)
-        {
-            if(k % 2 == 1) ans ^= d[i];
-            k >>= 1;
-        }
+        for(int i = 0; i < tot; i ++) if(k & (1ll << i)) ans ^= d[i];
+        return ans;
     }
 };
