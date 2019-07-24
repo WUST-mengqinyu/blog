ce3f0888e05dde5f8203e2a3dfd4bceeec931b88
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-07-24 10:02:33 +0800

    2019年 07月 24日 星期三 10:02:33 CST badcw

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd Team-Wiki && mkdocs gh-deploy
 cd /home/badcw/PycharmProjects/Team-Wiki && git pull
+cd Team-Wiki && mkdocs gh-deploy
 cd /home/badcw/PycharmProjects/Team-Wiki && git add *
 timenow=$(date)" badcw"
 cd /home/badcw/PycharmProjects/Team-Wiki && git commit -m "${timenow}"
