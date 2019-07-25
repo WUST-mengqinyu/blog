0b3cdcf56ccef2334657fe73bc9b8664a18284de
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-07-25 21:30:10 +0800

    2019年 07月 25日 星期四 21:30:10 CST badcw

---
+++ templates/05_String/07_Hash/01_hash.cpp
@@ -0,0 +1,18 @@
+typedef unsigned long long ull;
+struct mhash {
+    ull base[maxn];
+    ull hash_index[maxn];
+    ull seed; //31, 131
+    void inithash(ull seedt = 31) {
+        base[0] = 1;
+        seed = seedt;
+        for (int i = 1; i < maxn; ++i) base[i] = base[i - 1] * seed;
+    }
+    void hash(char *p, int n) { // from 1 to n
+        hash_index[0] = 0;
+        for (int i = 1; i <= n; ++i) hash_index[i] = hash_index[i - 1] * seed + p[i] - 'a';
+    }
+    ull gethash(int s, int e) {
+        return hash_index[e] - hash_index[s - 1] * base[e - s + 1];
+    }
+};
