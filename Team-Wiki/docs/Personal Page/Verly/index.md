0e5e56e85b2257a8d06e49cd4f93fa0b8684089f
Verly <Verly0206@gmail.com>
2019-07-29 00:54:14 +0800

    wiki update

--- Team-Wiki/docs/Personal Page/Verly/index.md
+++ Team-Wiki/docs/Personal Page/Verly/index.md
@@ -1,2 +1,27 @@
-啥也没有
+## Overview
+
+- 博客: [https://v-verly.github.io/](https://v-verly.github.io/)
+- CF: [Verly](https://codeforces.com/profile/Verly)
+- 有想详细写的题解丢在博客，wiki放个人训(mo)练(yu)记录
+- 这边写的东西应该不会很详细，康不懂的话私聊我一哈子
+
+## To-Do List
+
+- 补一下字符串(undo:exKMP, manachar, SA, SAM)
+- 切图论杂题，有空的时候学一些高级一点的东西
+- 以上两项都不想看的时候，去摸一下组合数学或者多项式
+- 有空开一个CF补题记录的page
+- 退役之前CF能不能打上黄名啊
+- 不能的话2000分行不行啊
+
+## Logs
+
+### Topic
+
+
+
+### Contest
+
+- 2019/7/27 KMP(done)
+- 2019/7/17 [网络流24题](../智力康复专题/网络流24题)(done)
 
--- templates/05_String/01_KMP/01_KMP.cpp
+++ templates/05_String/01_KMP/01_KMP.cpp
@@ -1,28 +1,27 @@
-const int maxn = 1e6 + 10;
+const int maxn=1e6+10;
 
-char a[maxn], b[maxn];
+char a[maxn],b[maxn];
 int nex[maxn];
 
 void getNext()
 {
-    int len = strlen(b), i = 0, j = -1;
+    int n = strlen(b), i = 0, j = -1;
     nex[i] = j;
-    for (int i = 1; i < len; i++)
+    while(i < n)
     {
-        while (j != -1 && b[i + 1] != b[j]) j = nex[j];
-        if (b[i] == b[j + 1]) j++;
-        nex[i] = j;
+        if(j == -1 || b[i] == b[j]) nex[++ i] = ++j;
+        else j = nex[j];
     }
 }
 
-void KMP()
+int KMP()
 {
     int n = strlen(a), m = strlen(b);
-    getNext();
-    int j = -1;
-    for (int i = 0; i < n; i++)
+    getNext(b);
+    int i = 0, j = 0;
+    while(i < n && j < m)
     {
-        while (j != -1 && a[i] != b[j + 1]) j = nex[j];
-        if (b[j + 1] == a[i]) j++;
+        if(j == -1 || a[i] == b[j]) i ++, j ++;
+        else j = nex[j];
     }
 }
