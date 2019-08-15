0b308e68035186c7bd4f549f1f8a1631212a08c1
WUST-mengqinyu <2017051104011@std.uestc.edu.cn>
2019-08-15 20:06:16 +0800

    2019年 08月 15日 星期四 20:06:16 CST badcw

--- templates/01_Math/06_卷积/02_NTT.cpp
+++ templates/01_Math/06_卷积/02_NTT.cpp
@@ -1,44 +1,83 @@
-const int p = 998244353;
-const int maxn = 3e6 + 10;
+const int maxn = 2097152;
+const int mod = 998244353;
+const int root = 3;
+// 998244353 -> 3, 1e9+7 -> 5,
 
-int n, m, limit = 1, l, r[maxn], G = 3, invG;
-ll a[maxn], b[maxn];
+template<long long mod, long long root>
+struct NTT {
+    vector<long long> omega;
 
-ll qp(ll a, ll n = p - 2)
-{
-	ll ans = 1, base = a;
-	for(; n; (base *= base) %= p, n >>= 1) if(n & 1) (ans *= base) %= p;
-	return ans;
-}
+    NTT() {
+        omega.resize(maxn + 1);
+        long long x = fpow(root, (mod - 1) / maxn);
+        omega[0] = 1ll;
+        for (int i = 1; i <= maxn; ++i)
+            omega[i] = omega[i - 1] * x % mod;
+    }
 
-void NTT(ll *A, int type)
-{
-	for(int i = 0; i < limit; i ++)
-		if(i < r[i]) swap(A[i], A[r[i]]);
-	for(int mid = 1; mid < limit; mid <<= 1)
-	{
-		ll Wn = qp(type == 1 ? G : invG, (p - 1) / (mid << 1));
-		for(int j = 0; j < limit; j += (mid << 1))
-		{
-			ll w = 1;
-			for(int k = 0; k < mid; k ++ , w = w * Wn % p)
-			{
-				int x = A[j + k], y = w * A[j + k + mid] % p;
-				A[j + k] = (x + y) % p;
-				A[j + k + mid] = (x - y + p) % p;
-			}
-		}
-	}
-}
+    long long fpow(long long a, long long n) {
+        (n += mod - 1) %= mod - 1;
+        long long r = 1;
+        for (; n; n >>= 1) {
+            if (n & 1) (r *= a) %= mod;
+            (a *= a) %= mod;
+        }
+        return r;
+    }
 
-void mul()
-{
-	while(limit <= n + m) limit <<= 1, l ++;
-	for(int i = 0; i < limit; i ++) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (l - 1));
-	NTT(a, 1);
-	NTT(b, 1);
-	for(int i = 0; i < limit; i ++) a[i] = (a[i] * b[i]) % p;
-	NTT(a, -1);
-	ll inv = qp(limit, p - 2);
-	for(int i = 0; i <= n + m; i ++) printf("%d ", a[i] * inv % p);
-}
+    void bitrev(vector<long long> &v, int n) {
+        int z = __builtin_ctz(n) - 1;
+        for (int i = 0; i < n; ++i) {
+            int x = 0;
+            for (int j = 0; j <= z; ++j) x ^= (i >> j & 1) << (z - j);
+            if (x > i) swap(v[x], v[i]);
+        }
+    }
+
+    void ntt(vector<long long> &v, int n) {
+        bitrev(v, n);
+        for (int s = 2; s <= n; s <<= 1) {
+            int z = s >> 1;
+            for (int i = 0; i < n; i += s) {
+                for (int k = 0; k < z; ++k) {
+                    long long x = v[i + k + z] * omega[maxn / s * k] % mod;
+                    v[i + k + z] = (v[i + k] + mod - x) % mod;
+                    (v[i + k] += x) %= mod;
+                }
+            }
+        }
+    }
+
+    void intt(vector<long long> &v, int n) {
+        ntt(v, n);
+        for (int i = 1; i < n / 2; ++i) swap(v[i], v[n - i]);
+        long long inv = fpow(n, -1);
+        for (int i = 0; i < n; ++i) (v[i] *= inv) %= mod;
+    }
+
+    vector<long long> operator()(vector<long long> a, vector<long long> b) {
+        int sz = 1;
+        while (sz < a.size() + b.size() - 1) sz <<= 1;
+        while (a.size() < sz) a.push_back(0);
+        while (b.size() < sz) b.push_back(0);
+        ntt(a, sz), ntt(b, sz);
+        vector<long long> c(sz);
+        for (int i = 0; i < sz; ++i) c[i] = a[i] * b[i] % mod;
+        intt(c, sz);
+        while (c.size() && c.back() == 0) c.pop_back();
+        return c;
+    }
+
+    vector<long long> operator()(vector<long long> a, int n) {
+        int sz = 1;
+        while (sz < n * a.size()) sz <<= 1;
+        while (a.size() < sz) a.push_back(0);
+        ntt(a, sz);
+        for (int i = 0; i < sz; ++i) a[i] = fpow(a[i], n);
+        intt(a, sz);
+        while (a.size() && a.back() == 0) a.pop_back();
+        return a;
+    }
+};
+
+NTT<mod, root> conv;
