719b966605c91d7093d9c8938f25d2e731d28627
Verly <Verly0206@gmail.com>
2019-09-26 19:03:43 +0800

    update

--- templates/00_Header/03_header/00_header.cpp
+++ templates/00_Header/03_header/00_header.cpp
@@ -36,3 +36,9 @@ freopen("data.in","r",stdin);
 freopen("data.out","w",stdout);
 clock_t ST = clock();
 cerr << "time: " << ((clock()-ST)*1000.0 / CLOCKS_PER_SEC) << "ms" << endl;
+
+__builtin_popcount(unsigned int n) //判断n的二进制有多少个1
+__builtin_parity(unsigned int n) //判断n的二进制中1的个数的奇偶性
+__builtin_ffs(unsigned int n) //判断n的二进制末尾最后一个1的位置，从一开始
+__builtin_ctz(unsigned int n) //判断n的二进制末尾后面0的个数，当n为0时，和n的类型有关
+__builtin_clz(unsigned int n) //返回前导0的个数
--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 cd /home/badcw/PycharmProjects/Team-Wiki && git pull
 cd Team-Wiki && mkdocs gh-deploy
 cd /home/badcw/PycharmProjects/Team-Wiki && git add *
-timenow=$(date)" badcw"
+timenow=$(date)
 cd /home/badcw/PycharmProjects/Team-Wiki && git commit -m "${timenow}"
 cd /home/badcw/PycharmProjects/Team-Wiki && git push
