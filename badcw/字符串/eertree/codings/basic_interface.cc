5478ac6e672bfc463deaddb908f3c0f2041dcf86
mengqinyu.01 <mengqinyu.01@bytedance.com>
2022-08-16 13:27:35 +0800

    Deployed f000cf16 with MkDocs version: 1.3.1

---
+++ badcw/字符串/eertree/codings/basic_interface.cc
@@ -0,0 +1,14 @@
+/**
+ * @brief eertree
+ *
+ */
+struct eertree {
+
+  /**
+   * @brief 往eertree末尾添加一个字符
+   *
+   * @param ch 添加的字符
+   * @return int 出现了多少个新的本质不同回文子串
+   */
+  int add(char ch) { todo; }
+};
---
+++ badcw/字符串/eertree/codings/basic_interface.rs
@@ -0,0 +1,5 @@
+struct eertree {
+    fn add(char ch) -> i32 {
+        todo!();
+    }
+}
